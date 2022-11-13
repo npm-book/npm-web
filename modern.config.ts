f88654d93ee4df5c2272954a814c2285268126b7
Beace <beaceshimin@gmail.com>
2022-11-13 22:12:20 +0800

    chore: init web framework

---
+++ modern.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from '@modern-js/app-tools';
+
+// https://modernjs.dev/docs/apis/app/config
+export default defineConfig({
+  runtime: {
+    router: true,
+    state: true,
+  },
+});
